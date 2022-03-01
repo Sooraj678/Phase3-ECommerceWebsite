adc8196cd96902788b8351083c8d78ff2cc29d46
Suraj Singh <Suraj Singh@DESKTOP-U7LG93U>
2022-03-01 22:28:15 +0530

    Registration Back- EndModule done...!!!!

--- ECommerceWebsite/src/main/java/com/ecommercewebsite/entities/User.java
+++ ECommerceWebsite/src/main/java/com/ecommercewebsite/entities/User.java
@@ -31,9 +31,12 @@ public class User {
 	
 	@Column(length = 1500, name = "user_address")
 	private String userAddress;
+	
+	@Column(name = "user_type")
+	private String userType;
 
 	public User(int userId, String userName, String userEmail, String userPassword, String userPhone, String userPic,
-			String userAddress) {
+			String userAddress, String userType) {
 		super();
 		this.userId = userId;
 		this.userName = userName;
@@ -42,10 +45,11 @@ public class User {
 		this.userPhone = userPhone;
 		this.userPic = userPic;
 		this.userAddress = userAddress;
+		this.userType = userType;
 	}
 
 	public User(String userName, String userEmail, String userPassword, String userPhone, String userPic,
-			String userAddress) {
+			String userAddress, String userType) {
 		super();
 		this.userName = userName;
 		this.userEmail = userEmail;
@@ -53,6 +57,7 @@ public class User {
 		this.userPhone = userPhone;
 		this.userPic = userPic;
 		this.userAddress = userAddress;
+		this.userType = userType;
 	}
 
 	public User() {
@@ -115,11 +120,20 @@ public class User {
 		this.userAddress = userAddress;
 	}
 
+	
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", userName=" + userName + ", userEmail=" + userEmail + ", userPassword="
 				+ userPassword + ", userPhone=" + userPhone + ", userPic=" + userPic + ", userAddress=" + userAddress
-				+ "]";
+				+ ", userType=" + userType + "]";
 	}
 	
 	
